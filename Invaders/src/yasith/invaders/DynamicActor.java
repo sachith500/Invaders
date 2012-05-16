669bc873ff50fb72d2be44d6b43df28ba6bbabd1
Yasith Vidanaarachchi <yasith@tuxv.net>
2012-05-15 23:26:52 -0400

    Add DynamicActor to handle the sprites (Ship/Invaders), document GameScreen

---
+++ Invaders/src/yasith/invaders/DynamicActor.java
@@ -0,0 +1,37 @@
+package yasith.invaders;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+
+/**
+ * An Actor created with a Sprite
+ */
+public class DynamicActor extends Actor {
+	
+	protected Sprite mSprite;
+	protected float mVelocity;
+
+	/**
+	 * Creates a new instance of a Dynamic Actor with the given sprite
+	 */
+	public DynamicActor(Sprite sprite){
+		mSprite = sprite;
+	}
+	
+	@Override
+	public void draw(SpriteBatch batch, float parentAlpha) {
+		// Prepare the sprite before drawing
+		mSprite.setPosition(x, y);
+		mSprite.setScale(scaleX, scaleY);
+		
+		// Draw the sprite
+		mSprite.draw(batch, parentAlpha);
+	}
+
+	@Override
+	public Actor hit(float x, float y) {
+		// We don't need touch detection for our Actors yet
+		return null;
+	}
+}
--- Invaders/src/yasith/invaders/GameScreen.java
+++ Invaders/src/yasith/invaders/GameScreen.java
@@ -1,13 +1,20 @@
 package yasith.invaders;
 
+/**
+ * Directs the Game Play
+ */
 public class GameScreen extends AbstractScreen {
 
+	/**
+	 * Creates a new GameScreen Instance
+	 */
 	public GameScreen() {
 		super();
 	}
 
 	@Override
 	public void show(){
+		// Calls the draw functions of its' Actors
 		super.show();
 	}
 }
