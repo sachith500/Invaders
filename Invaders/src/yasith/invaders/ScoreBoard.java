0075713d29a99e3d6697f453a8d05c32c42d1ee7
Yasith Vidanaarachchi <yasith@tuxv.net>
2012-05-31 21:05:16 -0400

    Implement Scoring System

---
+++ Invaders/src/yasith/invaders/ScoreBoard.java
@@ -0,0 +1,61 @@
+package yasith.invaders;
+
+/**
+ * Holds the score, achievement related data
+ */
+public class ScoreBoard {
+
+	
+	// Singleton instance
+	private static ScoreBoard sInstance = null;
+	
+	// Score for this session
+	private int mScore;
+	
+	// Highest score for all the games played
+	private int mHighScore;
+	
+	private ScoreBoard() {
+		mScore = 0;
+		loadHighScore();
+	}
+	
+	/**
+	 * Returns the Scoreboard instance
+	 */
+	public static synchronized ScoreBoard getInstance() {
+		if(sInstance == null){
+			sInstance = new ScoreBoard();
+		}
+		return sInstance;
+	}
+	
+	/**
+	 * Adds points to the score
+	 */
+	public void addPoints(int points) {
+		mScore += points;
+	}
+	
+	/**
+	 * Returns the score
+	 */
+	public int getScore() {
+		return mScore;
+	}
+	
+	/**
+	 * Resets the score
+	 */
+	public void reset(){
+		mScore = 0;
+	}
+	
+	/**
+	 * Loads and sets the High Score
+	 */
+	private void loadHighScore() {
+		// TODO implement loading logic
+		mHighScore = 0;
+	}
+}
